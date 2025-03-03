4b4456ddb839794a26a2e956da7729fdd99daef8
y24518 <hekou81@gmail.com>
2025-03-03 09:16:53 +0900

    Honpen対応

--- Honpen/Honpen.js
+++ Honpen/Honpen.js
@@ -1,7 +1,17 @@
+const GoKok = document.querySelector(".swi_Japa")
+const GoSan = document.querySelector(".swi_Math")
 const GoEng = document.querySelector(".swi_Eng")
 const GoSci = document.querySelector(".swi_Sci")
 const GoSya = document.querySelector(".swi_Sya")
 const Goret = document.querySelector(".swi_Ret")
+
+GoKok.addEventListener('click',function(){
+    window.location.href = './../kokugo/kokugo.html';
+},false);
+
+GoSan.addEventListener('click',function(){
+    window.location.href = './../Sansu/Sansu.html';
+},false);
 
 GoEng.addEventListener('click',function(){
     window.location.href = './../Eng/eng.html';
